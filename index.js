72b454033aa2fc144e1f34890b698852a692c0ff
ya95123 <ya95123@gmail.com>
2020-05-04 16:12:56 +0800

    版型OK

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ bot.on('message', async (event) => {
     // console.log(($('p').text()))
     // msg = $('p').text()
     // console.log(delHtmlTag(data[0].PageContent))
-    msg = `台灣時間：${delT(data[0].PagePublishTime)}\n地區：${delDot(data[0].PageSummary)}\n⭐ 近期消息\n「${data[0].PageTitle}」\n\n📨 主要內容\n${delHtmlTag(data[0].PageContent)}\n消息來源皆自：\n經濟部國際貿易局 經貿資訊網\nhttps://www.trade.gov.tw/World/List.aspx?code=7020&nodeID=45&areaID=4&country=b645Lit5ZyL5aSn6Zm4`
+    msg = `台灣時間：${delT(data[0].PagePublishTime)}\n地區：${delDot(data[0].PageSummary)}\n⭐ 近期消息 ⭐\n${data[0].PageTitle}\n\n📨 主要內容：${delHtmlTag(data[0].PageContent)}\n消息來源皆自：\n經濟部國際貿易局 經貿資訊網\nhttps://www.trade.gov.tw/World/List.aspx?code=7020&nodeID=45&areaID=4&country=b645Lit5ZyL5aSn6Zm4`
   } catch (error) {
     msg = '發生錯誤'
   }
