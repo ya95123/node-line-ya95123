21d0c9b99deaddfdd5dd58ee66769c859682029a
ya95123 <ya95123@gmail.com>
2020-05-23 18:33:15 +0800

    目錄 分段查詢 test9

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ bot.on('message', async (event) => {
           msg[count] = `第 ${i + 1} 則\n台灣時間：${delLine(delT(data[i].PagePublishTime))}\n地區：${delDot(data[i].PageSummary)}\n⭐ 最新消息\n${data[i].PageTitle}\n\n📨 主要內容\n${delSpace(delHtmlTag(data[i].PageContent))}`
         }
       }
-    } else if (use === '目錄' || use === 'c' || use === 'C') {
+    } else if (use.includes('目錄') || use.includes('c') || use.includes('C')) {
       // *呼叫目錄 c OK
       // 總數分四段，且無條件捨去
       const divide4 = Math.floor((data.length / 4))
